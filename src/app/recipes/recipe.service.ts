0a9e31ef11797514c4f0b5f7c9e7a348693927e1
Horia Negreanu <horia.negreanu@yahoo.com>
2023-04-03 13:05:55 +0300

    Section 12

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -18,6 +18,10 @@ export class RecipeService {
         return this.recipes.slice(); //returns a copy
     }
 
+    getRecipe(index: number){
+        return this.recipes[index];
+    }
+
     addIngredientsToShoppingList(ingredients: Ingredient[]){
         this.slService.addIngredients(ingredients);
     }
