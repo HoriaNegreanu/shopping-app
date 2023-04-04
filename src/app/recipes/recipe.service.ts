7da17c68fdfb6f824d6a88e8eaeef5b3b3b6c923
Horia Negreanu <horia.negreanu@yahoo.com>
2023-04-04 13:02:48 +0300

    Section 14

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,11 @@
-import { EventEmitter, Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
+import { Subject } from "rxjs";
 
 @Injectable()
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
         new Recipe('Schnitzel', 'Tasty Schnitzel', 'http://www.cheap-chef.com/wp-content/uploads/2015/04/bakedspaghetti1-610x406.jpg',[new Ingredient('Meat',1), new Ingredient('French Fries',20)]),
