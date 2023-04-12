860f1b55bde86d028f78315c37bfea6e65bf43d7
Horia Negreanu <horia.negreanu@yahoo.com>
2023-04-12 14:44:59 +0300

    Section 16

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -6,23 +6,39 @@ import { Subject } from "rxjs";
 
 @Injectable()
 export class RecipeService {
+    recipesChanged = new Subject<Recipe[]>();
 
     private recipes: Recipe[] = [
-        new Recipe('Schnitzel', 'Tasty Schnitzel', 'http://www.cheap-chef.com/wp-content/uploads/2015/04/bakedspaghetti1-610x406.jpg',[new Ingredient('Meat',1), new Ingredient('French Fries',20)]),
-        new Recipe('Burger', 'Huge burger', 'http://www.cheap-chef.com/wp-content/uploads/2015/04/bakedspaghetti1-610x406.jpg',[new Ingredient('Buns',2), new Ingredient('Meat',1)])
+        new Recipe('Schnitzel', 'Tasty Schnitzel', 'http://www.cheap-chef.com/wp-content/uploads/2015/04/bakedspaghetti1-610x406.jpg', [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]),
+        new Recipe('Burger', 'Huge burger', 'http://www.cheap-chef.com/wp-content/uploads/2015/04/bakedspaghetti1-610x406.jpg', [new Ingredient('Buns', 2), new Ingredient('Meat', 1)])
     ];
 
-    constructor(private slService: ShoppingListService){}
+    constructor(private slService: ShoppingListService) { }
 
-    getRecipes(){
+    getRecipes() {
         return this.recipes.slice(); //returns a copy
     }
 
-    getRecipe(index: number){
+    getRecipe(index: number) {
         return this.recipes[index];
     }
 
-    addIngredientsToShoppingList(ingredients: Ingredient[]){
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
         this.slService.addIngredients(ingredients);
+    }
+
+    addRecipe(recipe: Recipe) {
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    updateRecipe(index: number, newRecipe: Recipe) {
+        this.recipes[index] = newRecipe;
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    deleteRecipe(index: number) {
+        this.recipes.splice(index, 1);
+        this.recipesChanged.next(this.recipes.slice());
     }
 }
